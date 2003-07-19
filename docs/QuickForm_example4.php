645fbe5bf4b02b488b8433ef554071e170e63509
Bertrand Mansion <mansion@php.net>
2003-07-19 09:30:09 +0000

    Use exportValue().
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_QuickForm/trunk@135420 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/QuickForm_example4.php
+++ docs/QuickForm_example4.php
@@ -11,15 +11,17 @@ $form = new HTML_QuickForm('frmTest', 'GET');
 $form->addElement('text', 'itxtTest', 'Test Text to trim:');
 $form->addElement('submit', 'submit', 'submit');
 $form->addRule('itxtTest', 'Test text is required', 'required');
+
+// In this example, filter is used BEFORE the validation.
 $form->applyFilter('__ALL__', 'trim');
 
 if ($form->validate()) {
     $form->freeze();
     echo 'Before filter:<pre>';
-    echo $form->getElementValue('itxtTest');
+    var_dump($form->getElementValue('itxtTest'));
     echo '</pre>';
     echo 'After filter:<pre>';
-    var_dump($form->_submitValues['itxtTest']);
+    var_dump($form->exportValue('itxtTest'));
     echo '</pre>';
 }
 $form->display();
