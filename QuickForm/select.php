ca2205a8af85d21762cf24bf05c5dc9797ec12d1
Alexey Borzov <avb@php.net>
2005-06-18 20:54:51 +0000

    Fix bug #3193: frozen select elements could display incorrect value
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_QuickForm/trunk@188743 c90b9560-bf6c-de11-be94-00142212c4b1

--- QuickForm/select.php
+++ QuickForm/select.php
@@ -512,7 +512,7 @@ class HTML_QuickForm_select extends HTML_QuickForm_element {
         if (is_array($this->_values)) {
             foreach ($this->_values as $key => $val) {
                 for ($i = 0, $optCount = count($this->_options); $i < $optCount; $i++) {
-                    if ($val == $this->_options[$i]['attr']['value']) {
+                    if ((string)$val == (string)$this->_options[$i]['attr']['value']) {
                         $value[$key] = $this->_options[$i]['text'];
                         break;
                     }
