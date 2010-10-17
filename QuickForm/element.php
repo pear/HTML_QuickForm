8d3f7a0a96bcea28ba04a29b1ad5594a58d1e389
Alexey Borzov <avb@php.net>
2010-10-17 07:31:18 +0000

    Copyright year update, prepare release 3.2.12
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_QuickForm/trunk@304451 c90b9560-bf6c-de11-be94-00142212c4b1

--- QuickForm/element.php
+++ QuickForm/element.php
@@ -17,7 +17,7 @@
  * @author      Adam Daniel <adaniel1@eesus.jnj.com>
  * @author      Bertrand Mansion <bmansion@mamasam.com>
  * @author      Alexey Borzov <avb@php.net>
- * @copyright   2001-2009 The PHP Group
+ * @copyright   2001-2010 The PHP Group
  * @license     http://www.php.net/license/3_01.txt PHP License 3.01
  * @version     CVS: $Id$
  * @link        http://pear.php.net/package/HTML_QuickForm
